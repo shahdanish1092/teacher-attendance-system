5d5cf43cd189b6076d1b38f7d49628cdd4936bfb
shahdanish1092 <danishshah9749@gmail.com>
2025-11-01 21:11:17 +0530

    Deployment ready with Procfile and updates

--- app.py
+++ app.py
@@ -206,7 +206,9 @@ def upload_face():
         return jsonify({"message": f"Error: {e}"}), 500
 
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
